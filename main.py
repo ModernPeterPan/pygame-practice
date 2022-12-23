541d27b24403c3ed6d28d78a79f19404be4c7a1f
Marcos Lopez <marcos.s.lopez.137@gmail.com>
2022-12-23 12:10:29 -0600

    Added a nice snail surface.

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ sky_surface = pygame.image.load('graphics/Sky.png')
 ground_surface = pygame.image.load('graphics/ground.png')
 text_surface = test_font.render('My game', False, 'Green')
 
+snail_surface = pygame.image.load('graphics/snail/snail1.png')
+snail_x_pos = 600
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -19,7 +22,8 @@ while True:
 
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
-    screen.blit(text_surface,(300, 50))
+    screen.blit(text_surface, (300, 50))
+    screen.blit(snail_surface, (snail_x_pos, 250))
 
     pygame.display.update()
     clock.tick(60)
